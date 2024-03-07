b4f0e1c1eb55eeebdc63a9c04d52af1250949ad9
Shivani9698 <119753029+Shivani9698@users.noreply.github.com>
2024-03-07 13:17:44 +0530

    Update Working code.ino

--- Working code.ino
+++ Working code.ino
@@ -1,17 +1,18 @@
+
 #define GLED1_pin 2
 #define GLED2_pin 5
 #define GLED3_pin 15
 #define GLED4_pin 4
-#define GLED5_pin 17
 
 #define iron_pin 16
 
 
-#define RLED1_pin 32
+#define RLED1_pin 21
 #define RLED2_pin 33
 #define RLED3_pin 22
 #define RLED4_pin 23
-#define RLED5_pin 21
+#define RLED5_pin 18
+
 
 
 #define Phase_pin 25
@@ -25,15 +26,17 @@ void setup() {
 
   pinMode(GLED1_pin, OUTPUT);
   pinMode(GLED2_pin, OUTPUT);
-    pinMode(GLED3_pin, OUTPUT);
+  pinMode(GLED3_pin, OUTPUT);
   pinMode(GLED4_pin, OUTPUT);
-    pinMode(GLED5_pin, OUTPUT);
+
 
   pinMode(iron_pin, OUTPUT);
+  digitalWrite(RLED1_pin, OUTPUT);
   pinMode(RLED2_pin, OUTPUT);
     pinMode(RLED3_pin, OUTPUT);
       pinMode(RLED4_pin, OUTPUT);
         pinMode(RLED5_pin, OUTPUT);
+        
         
 
   pinMode(Phase_pin, INPUT_PULLUP);
@@ -43,91 +46,116 @@ void setup() {
   pinMode(Hand_pin, INPUT_PULLUP);
 }
 
+
 void loop() {
-  if ((digitalRead(Phase_pin) == LOW) && (digitalRead(Neutral_pin) == LOW) &&
+
+  if ((digitalRead(Phase_pin) == HIGH) && (digitalRead(Neutral_pin) == HIGH) &&
       (digitalRead(Earth_pin) == HIGH) && (digitalRead(Shoe_pin) == HIGH) && (digitalRead(Hand_pin) == HIGH)) {
     // Iron on, all off
     Serial.println("I am in IF condition");
-    digitalWrite(iron_pin, HIGH);
-    
-   digitalWrite(RLED2_pin, LOW);
+    digitalWrite(iron_pin, LOW);
+    digitalWrite(RLED1_pin, LOW);
+    digitalWrite(RLED2_pin, LOW);
     digitalWrite(RLED3_pin, LOW);
     digitalWrite(RLED4_pin, LOW);
-    digitalWrite(RLED4_pin, LOW);
-
+    digitalWrite(RLED5_pin, LOW);
     digitalWrite(GLED1_pin, LOW);
     digitalWrite(GLED2_pin, LOW);
     digitalWrite(GLED3_pin, LOW);
     digitalWrite(GLED4_pin, LOW);
-    digitalWrite(GLED5_pin, LOW);
-
-    
-
+  }
+  else if ((digitalRead(Phase_pin) == LOW) && (digitalRead(Neutral_pin) == LOW) &&
+           (digitalRead(Earth_pin) == HIGH) && (digitalRead(Shoe_pin) == HIGH) && (digitalRead(Hand_pin) == HIGH)) {
+    // Iron on, all off
+    Serial.println("I am in IF condition");
+    digitalWrite(iron_pin, HIGH);
+    digitalWrite(RLED1_pin, LOW);
+    digitalWrite(RLED2_pin, LOW);
+    digitalWrite(RLED3_pin, LOW);
+    digitalWrite(RLED4_pin, LOW);
+    digitalWrite(RLED5_pin, LOW);
+    digitalWrite(GLED1_pin, LOW);
+    digitalWrite(GLED2_pin, LOW);
+    digitalWrite(GLED3_pin, LOW);
+    digitalWrite(GLED4_pin, LOW);
   } 
   else if ((digitalRead(Phase_pin) == LOW) && (digitalRead(Neutral_pin) == LOW) &&
            (digitalRead(Earth_pin) == HIGH) && (digitalRead(Shoe_pin) == HIGH) && (digitalRead(Hand_pin) == LOW)) {
     // All Red Light on AND Iron on
     Serial.println("ALL RED LIGHT ON AND IRON ON");
 
-     digitalWrite(iron_pin, HIGH);
+    digitalWrite(iron_pin, HIGH);
     
-    digitalWrite(RLED1_pin, HIGH);
+   
+      digitalWrite(RLED1_pin, HIGH);
+      delay(100);
+      digitalWrite(RLED1_pin, LOW);
+      delay(100);
+    
     digitalWrite(RLED2_pin, HIGH);
-    digitalWrite(RLED3_pin, HIGH);
-    digitalWrite(RLED4_pin, HIGH);
-    digitalWrite(RLED4_pin, HIGH);
-
-    digitalWrite(GLED1_pin, LOW);
-    digitalWrite(GLED2_pin, LOW);
-    digitalWrite(GLED3_pin, LOW);
-    digitalWrite(GLED4_pin, LOW);
-    digitalWrite(GLED5_pin, LOW);
-  } 
-  else if ((digitalRead(Phase_pin) == LOW) && (digitalRead(Neutral_pin) == LOW) &&
-           (digitalRead(Earth_pin) == HIGH) && (digitalRead(Shoe_pin) == LOW) && (digitalRead(Hand_pin) == LOW)) {
-    // All Red Light off on AND Iron on
-    Serial.println("All Red Light off on AND Iron on");
-
-         digitalWrite(iron_pin, HIGH);
-    
-    digitalWrite(RLED1_pin, LOW);
+    delay(100);
     digitalWrite(RLED2_pin, LOW);
+      delay(100);
+    digitalWrite(RLED3_pin, HIGH);
+    delay(100);
     digitalWrite(RLED3_pin, LOW);
+      delay(100);
+    digitalWrite(RLED4_pin, HIGH);
+    delay(100);
     digitalWrite(RLED4_pin, LOW);
-    digitalWrite(RLED4_pin, LOW);
+      delay(100);
+    digitalWrite(RLED5_pin, HIGH);
+    delay(100);
+    digitalWrite(RLED5_pin, LOW);
+      delay(100);
+    
 
     digitalWrite(GLED1_pin, LOW);
     digitalWrite(GLED2_pin, LOW);
     digitalWrite(GLED3_pin, LOW);
     digitalWrite(GLED4_pin, LOW);
-    digitalWrite(GLED5_pin, LOW);
   } 
   else if ((digitalRead(Phase_pin) == LOW) && (digitalRead(Neutral_pin) == LOW) &&
            (digitalRead(Earth_pin) == LOW) && (digitalRead(Shoe_pin) == LOW) && (digitalRead(Hand_pin) == LOW)) {
     // All green on and on AND Iron on
     Serial.println("All green on and on AND Iron on");
-     digitalWrite(iron_pin, HIGH);
+    digitalWrite(iron_pin, HIGH);
      
+    
       digitalWrite(GLED1_pin, HIGH);
+      delay(100);
+      digitalWrite(GLED1_pin, LOW);
+      delay(100);
+    
     digitalWrite(GLED2_pin, HIGH);
+    delay(100);
+     digitalWrite(GLED2_pin, LOW);
+      delay(100);
+    
     digitalWrite(GLED3_pin, HIGH);
+    delay(100);
+     digitalWrite(GLED3_pin, LOW);
+      delay(100);
+    
     digitalWrite(GLED4_pin, HIGH);
-    digitalWrite(GLED5_pin, HIGH);
-  } 
+    delay(100);
+     digitalWrite(GLED4_pin, LOW);
+      delay(100);
+    
+  }
+            
   else {
     Serial.println("I am in else condition");
-        digitalWrite(RLED1_pin, LOW);
+    digitalWrite(RLED1_pin, LOW);
     digitalWrite(RLED2_pin, LOW);
     digitalWrite(RLED3_pin, LOW);
     digitalWrite(RLED4_pin, LOW);
-    digitalWrite(RLED4_pin, LOW);
-
+    digitalWrite(RLED5_pin, LOW);
     digitalWrite(GLED1_pin, LOW);
     digitalWrite(GLED2_pin, LOW);
     digitalWrite(GLED3_pin, LOW);
     digitalWrite(GLED4_pin, LOW);
-    digitalWrite(GLED5_pin, LOW);
   }
 
-  delay(1000); // Adjust the delay if needed
+  delay(100); // Adjust the delay if needed
 }
